c5c73c409a345d774b10d69005dd0a4135cf5317
melanie <yjw9700@agape.hanyang.ac.kr>
2023-06-07 17:36:45 +0900

    23.06.07: title 로만 item rep, linear_output

--- model.py
+++ model.py
@@ -114,8 +114,8 @@ class MovieExpertCRS(nn.Module):
         # Loss
         self.criterion = nn.CrossEntropyLoss()
         # self.linear_output = nn.Linear(self.token_emb_dim, 6923).requires_grad_(False)
-        self.freezed_item_rep = nn.Parameter(self.token_emb_dim, 6923, requires_grad=False)
-        # self.unfreezed_item_rep = nn.Parameter(self.token_emb_dim, 6923, requires_grad=True)
+        # self.freezed_item_rep = nn.Parameter(self.token_emb_dim, 6923, requires_grad=False)
+        self.unfreezed_item_rep = nn.Parameter(self.token_emb_dim, 6923, requires_grad=True)
 
 
         # initialize all parameter (except for pretrained BERT)
@@ -275,6 +275,6 @@ class MovieExpertCRS(nn.Module):
         # gate = torch.sigmoid(self.gating(torch.cat([token_attn_rep, entity_attn_rep], dim=1)))
         user_embedding = token_attn_rep
         # item_rep = self.item_representations()
-        scores = F.linear(user_embedding, self.freezed_item_rep)
+        scores = F.linear(user_embedding, self.unfreezed_item_rep)
         # scores = self.linear_output(user_embedding)
         return scores
